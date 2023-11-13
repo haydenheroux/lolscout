1bd64f91de1dcd67eebff8003e3e098f3a0aee35
Hayden Heroux <haydenheroux@gmail.com>
2023-11-13 13:20:03 -0500

    refactor(tui): Add tui package.

--- main.go
+++ main.go
@@ -2,28 +2,16 @@ package main
 
 import (
 	"fmt"
+	"lolscout/tui"
 
 	"github.com/KnutZuidema/golio"
 	"github.com/KnutZuidema/golio/api"
 	env "github.com/Netflix/go-env"
-	"github.com/charmbracelet/lipgloss"
 	log "github.com/sirupsen/logrus"
 )
 
 type Environment struct {
 	RiotApiKey string `env:"RIOT_API_KEY"`
-}
-
-type MatchParticipant struct {
-	ChampionName      string
-	Level             int
-	Kills             int
-	Deaths            int
-	Assists           int
-	KillParticipation float64
-	CS                int
-	CSPerMinute       float64
-	Win               bool
 }
 
 func main() {
@@ -47,7 +35,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var summonerMatchParticipants []MatchParticipant
+	var summonerMatchParticipants []tui.MatchParticipant
 
 	for _, matchId := range matchIds {
 		match, err := client.Riot.LoL.Match.Get(matchId)
@@ -65,7 +53,7 @@ func main() {
 
 		for _, participant := range match.Info.Participants {
 			if participant.PUUID == summoner.PUUID {
-				var matchParticipant MatchParticipant
+				var matchParticipant tui.MatchParticipant
 
 				matchParticipant.ChampionName = participant.ChampionName
 				matchParticipant.Level = participant.ChampLevel
@@ -83,98 +71,6 @@ func main() {
 	}
 
 	for _, matchParticipant := range summonerMatchParticipants {
-		var backgroundColor, borderColor lipgloss.Color
-
-		if matchParticipant.Win {
-			backgroundColor = lipgloss.Color("#28344E")
-			borderColor = lipgloss.Color("#5383E8")
-		} else {
-			backgroundColor = lipgloss.Color("#59343B")
-			borderColor = lipgloss.Color("#E84057")
-		}
-
-		// TODO
-		championSectionWidth := 14
-
-		championName := lipgloss.NewStyle().Background(backgroundColor).Bold(true).Width(championSectionWidth)
-
-		renderedChampionName := championName.Render(matchParticipant.ChampionName)
-
-		levelString := fmt.Sprintf("Lvl. %d", matchParticipant.Level)
-
-		level := lipgloss.NewStyle().Background(backgroundColor).PaddingRight(1).Width(championSectionWidth)
-
-		renderedLevel := level.Render(levelString)
-
-		renderedChampionSection := lipgloss.JoinVertical(lipgloss.Left, renderedChampionName, renderedLevel)
-
-		kdaValue := float64(matchParticipant.Kills+matchParticipant.Assists) / float64(matchParticipant.Deaths)
-
-		whiteKda := lipgloss.NewStyle().Background(backgroundColor)
-
-		goldKda := whiteKda.Copy().Foreground(lipgloss.Color("#FF8200"))
-		blueKda := goldKda.Copy().Foreground(lipgloss.Color("#0093FF"))
-		greenKda := goldKda.Copy().Foreground(lipgloss.Color("#00BBA3"))
-
-		kdaTextStyle := whiteKda
-
-		if kdaValue >= 5 {
-			kdaTextStyle = goldKda
-		} else if kdaValue >= 4 {
-			kdaTextStyle = blueKda
-		} else if kdaValue >= 3 {
-			kdaTextStyle = greenKda
-		}
-
-		kdaTextStyle = kdaTextStyle.PaddingRight(1)
-
-		kdaText := fmt.Sprintf("%.2f:1 KDA", kdaValue)
-
-		renderedKdaText := kdaTextStyle.Render(kdaText)
-
-		killParticipationString := fmt.Sprintf("(%.0f%% KP)", matchParticipant.KillParticipation*100)
-
-		killParticipation := lipgloss.NewStyle().Background(backgroundColor).PaddingRight(1)
-
-		if matchParticipant.KillParticipation >= 0.5 {
-			killParticipation = killParticipation.Foreground(lipgloss.Color("#E84057"))
-		}
-
-		renderedKillParticipation := killParticipation.Render(killParticipationString)
-
-		renderedKdaBottomSection := lipgloss.JoinHorizontal(lipgloss.Bottom, renderedKdaText, renderedKillParticipation)
-
-		kdaString := fmt.Sprintf("%d/%d/%d", matchParticipant.Kills, matchParticipant.Deaths, matchParticipant.Assists)
-
-		kda := lipgloss.NewStyle().Background(backgroundColor).Bold(true).Width(lipgloss.Width(renderedKdaBottomSection))
-
-		renderedKda := kda.Render(kdaString)
-
-		// TODO Center renderedKda above bottom section
-		renderedKdaSection := lipgloss.JoinVertical(lipgloss.Left, renderedKda, renderedKdaBottomSection)
-
-		csString := fmt.Sprintf("%d CS", matchParticipant.CS)
-
-		csPerMinuteString := fmt.Sprintf("%.1f CS/M", matchParticipant.CSPerMinute)
-
-		csPerMinute := lipgloss.NewStyle().Background(backgroundColor)
-
-		if matchParticipant.CSPerMinute >= 8.0 {
-			csPerMinute = csPerMinute.Foreground(lipgloss.Color("#E84057"))
-		}
-
-		renderedCsPerMinute := csPerMinute.Render(csPerMinuteString)
-
-		cs := lipgloss.NewStyle().Background(backgroundColor).Bold(true).Width(lipgloss.Width(renderedCsPerMinute))
-
-		renderedCs := cs.Render(csString)
-
-		renderedCsSection := lipgloss.JoinVertical(lipgloss.Left, renderedCs, renderedCsPerMinute)
-
-		renderedBody := lipgloss.JoinHorizontal(lipgloss.Top, renderedChampionSection, renderedKdaSection, renderedCsSection)
-
-		container := lipgloss.NewStyle().Background(backgroundColor).Border(lipgloss.BlockBorder(), false, false, false, true).BorderForeground(borderColor).Padding(2).MarginBottom(1).Width(80)
-
-		fmt.Println(container.Render(lipgloss.NewStyle().Background(backgroundColor).Render(renderedBody)))
+		fmt.Println(tui.RenderMatchParticipant(matchParticipant))
 	}
 }
---
+++ tui/render.go
@@ -0,0 +1,115 @@
+package tui
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+type MatchParticipant struct {
+	ChampionName      string
+	Level             int
+	Kills             int
+	Deaths            int
+	Assists           int
+	KillParticipation float64
+	CS                int
+	CSPerMinute       float64
+	Win               bool
+}
+
+func RenderMatchParticipant(matchParticipant MatchParticipant) string {
+	var backgroundColor, borderColor lipgloss.Color
+
+	if matchParticipant.Win {
+		backgroundColor = blueBackgroundColor
+		borderColor = blueBorderColor
+	} else {
+		backgroundColor = redBackgroundColor
+		borderColor = redBorderColor
+	}
+
+	// TODO
+	championSectionWidth := 14
+
+	championName := lipgloss.NewStyle().Background(backgroundColor).Bold(true).Width(championSectionWidth)
+
+	renderedChampionName := championName.Render(matchParticipant.ChampionName)
+
+	levelString := fmt.Sprintf("Lvl. %d", matchParticipant.Level)
+
+	level := lipgloss.NewStyle().Background(backgroundColor).PaddingRight(1).Width(championSectionWidth)
+
+	renderedLevel := level.Render(levelString)
+
+	renderedChampionSection := lipgloss.JoinVertical(lipgloss.Left, renderedChampionName, renderedLevel)
+
+	kdaValue := float64(matchParticipant.Kills+matchParticipant.Assists) / float64(matchParticipant.Deaths)
+
+	whiteKda := lipgloss.NewStyle().Background(backgroundColor)
+
+	goldKda := whiteKda.Copy().Foreground(goldColor)
+	blueKda := goldKda.Copy().Foreground(blueColor)
+	greenKda := goldKda.Copy().Foreground(greenColor)
+
+	kdaTextStyle := whiteKda
+
+	if kdaValue >= 5 {
+		kdaTextStyle = goldKda
+	} else if kdaValue >= 4 {
+		kdaTextStyle = blueKda
+	} else if kdaValue >= 3 {
+		kdaTextStyle = greenKda
+	}
+
+	kdaTextStyle = kdaTextStyle.PaddingRight(1)
+
+	kdaText := fmt.Sprintf("%.2f:1 KDA", kdaValue)
+
+	renderedKdaText := kdaTextStyle.Render(kdaText)
+
+	killParticipationString := fmt.Sprintf("(%.0f%% KP)", matchParticipant.KillParticipation*100)
+
+	killParticipation := lipgloss.NewStyle().Background(backgroundColor).PaddingRight(1)
+
+	if matchParticipant.KillParticipation >= 0.5 {
+		killParticipation = killParticipation.Foreground(redColor)
+	}
+
+	renderedKillParticipation := killParticipation.Render(killParticipationString)
+
+	renderedKdaBottomSection := lipgloss.JoinHorizontal(lipgloss.Bottom, renderedKdaText, renderedKillParticipation)
+
+	kdaString := fmt.Sprintf("%d/%d/%d", matchParticipant.Kills, matchParticipant.Deaths, matchParticipant.Assists)
+
+	kda := lipgloss.NewStyle().Background(backgroundColor).Bold(true).Width(lipgloss.Width(renderedKdaBottomSection))
+
+	renderedKda := kda.Render(kdaString)
+
+	// TODO Center renderedKda above bottom section
+	renderedKdaSection := lipgloss.JoinVertical(lipgloss.Left, renderedKda, renderedKdaBottomSection)
+
+	csString := fmt.Sprintf("%d CS", matchParticipant.CS)
+
+	csPerMinuteString := fmt.Sprintf("%.1f CS/M", matchParticipant.CSPerMinute)
+
+	csPerMinute := lipgloss.NewStyle().Background(backgroundColor)
+
+	if matchParticipant.CSPerMinute >= 8.0 {
+		csPerMinute = csPerMinute.Foreground(redColor)
+	}
+
+	renderedCsPerMinute := csPerMinute.Render(csPerMinuteString)
+
+	cs := lipgloss.NewStyle().Background(backgroundColor).Bold(true).Width(lipgloss.Width(renderedCsPerMinute))
+
+	renderedCs := cs.Render(csString)
+
+	renderedCsSection := lipgloss.JoinVertical(lipgloss.Left, renderedCs, renderedCsPerMinute)
+
+	renderedBody := lipgloss.JoinHorizontal(lipgloss.Top, renderedChampionSection, renderedKdaSection, renderedCsSection)
+
+	container := lipgloss.NewStyle().Background(backgroundColor).Border(lipgloss.BlockBorder(), false, false, false, true).BorderForeground(borderColor).Padding(2).MarginBottom(1).Width(80)
+
+	return container.Render(lipgloss.NewStyle().Background(backgroundColor).Render(renderedBody))
+}
